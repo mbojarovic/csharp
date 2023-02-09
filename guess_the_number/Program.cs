2517e673f489b926c096d3225bb18f350ee7c4bc
Marek <m.bojarovic@gmail.com>
2023-02-09 21:23:21 +0200

    первый код на c#

---
+++ guess_the_number/Program.cs
@@ -0,0 +1,23 @@
+﻿string randomNumber = new Random().Next(1, 10).ToString();
+Console.WriteLine("угадайте число от 1 до 10 ");
+while (true) {
+    Console.Write("напишите число: ");
+    string userNumber = Console.ReadLine();
+
+    if (randomNumber == userNumber) {
+        Console.WriteLine("Вы угадали!!!");
+        Console.WriteLine("хотите сыграть заново? да или нет");
+        string userAnswer = Console.ReadLine();
+
+        if (userAnswer == "да") {
+            continue;
+        } else if (userAnswer == "нет") {
+            break;
+            } else {
+            Console.WriteLine("eeeeeeee");
+        }
+
+    } else {
+        Console.WriteLine("вы не угадали!!!");
+    }
+}
