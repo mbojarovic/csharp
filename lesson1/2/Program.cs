e1d0baea06b2e2b4ada985bce789da9d46d64a4c
Marek <m.bojarovic@gmail.com>
2023-02-11 19:05:43 +0200

    Задача 2 final v

--- lesson1/2/Program.cs
+++ lesson1/2/Program.cs
@@ -1,15 +1,5 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // function maxMinNumber(int a, int b)
-// {
-//     Console.WriteLine("1");
-//     int firstNumber = Convert.To.Int32(Console.ReadLine());
-//     Console.WriteLine("2");
-//     int secondNumber = Convert.To.Int32(Console.ReadLine());
-
-//     return firstNumber + secondNumber;
-// }
-
-// maxMinNumber(2, 5);
 Console.Clear();
 Console.WriteLine("максимальное минимальное число из 2 цифр.");
 Console.Write("введите первое число: ");
