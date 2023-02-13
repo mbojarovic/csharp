8dfe2b040b95a9797d90abba29de5d23e10d4e88
Marek <m.bojarovic@gmail.com>
2023-02-13 20:23:32 +0200

    Задача 10

---
+++ lesson2/10/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+//int randomNumber = ;
+int randomNumber = new Random().Next(99,1000);
+int rNumber = randomNumber / 10 % 10;
+Console.WriteLine($"число {randomNumber}вторую цифру{rNumber}");
