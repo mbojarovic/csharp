d4dda54c3ff0da75179cd89e8d873a37088276d1
Marek <m.bojarovic@gmail.com>
2023-02-13 21:44:17 +0200

    Задача 13

--- lesson2/10/Program.cs
+++ lesson2/10/Program.cs
@@ -5,6 +5,4 @@
 782 -> 8
 918 -> 1 */
 //int randomNumber = ;
-int randomNumber = new Random().Next(99,1000);
-int rNumber = randomNumber / 10 % 10;
-Console.WriteLine($"число {randomNumber} вторую цифру {rNumber}");
+
