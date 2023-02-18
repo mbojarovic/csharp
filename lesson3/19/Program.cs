f11c874b1f794ba34060a5958c9799b1718e1b08
Marek <m.bojarovic@gmail.com>
2023-02-18 23:29:41 +0200

    Задача 19

---
+++ lesson3/19/Program.cs
@@ -0,0 +1,86 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+ Через строку решать нельзя.
+
+14212 -> нет
+12821 -> да
+23432 -> да*/
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    int msg = Convert.ToInt32(Console.ReadLine());
+    return msg;
+}
+
+bool validateNumber(int n)
+{
+    if (n < 99999 && n > 9999)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isPalindrome(int item)
+{
+    int rev = 0;
+    int rem = 0;
+    int num = item;
+
+    while (num > 0)
+    {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    if (rev == item)
+        return true;
+    else
+        return false;
+}
+
+while (true)
+{
+    try
+    {
+        int number = prompt("введите пятизначное число:");
+
+        if (validateNumber(number))
+        {
+            if (isPalindrome(number))
+            {
+                Console.WriteLine($"является ли {number} палиндромом -> да");
+            }
+            else
+            {
+                Console.WriteLine($"является ли {number} палиндромом -> нет");
+            };
+
+            Console.WriteLine("Проверить еще одно число yes or no ?");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes")
+                continue;
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("введено ни пятизначное число!");
+        }
+
+
+    }
+
+    catch
+    {
+        Console.WriteLine("допустимы только цифры");
+    }
+}
+
+
