84c33d92b2e04398763cdb9f9038332e526514c1
Marek <m.bojarovic@gmail.com>
2023-02-20 23:24:58 +0200

    Задача 23

---
+++ lesson3/21Hard/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 21 - HARD необязательная
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве.
+ Сначала задается N с клавиатуры, потом задаются координаты точек.*/
+
+ /*Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в n пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+// void Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     double n = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+//     Console.WriteLine($"Расстояние составляет {n}");
+// }
+
+// Console.WriteLine("Введите координату х1 и у1 и z1");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату х2 и у2 и z2");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// Distance(x1, y1, z1, x2, y2, z2);
---
+++ lesson3/23/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+int prompt(string message)
+{
+    Console.Write(message);
+    int msg = Convert.ToInt32(Console.ReadLine());
+    return msg;
+}
+
+int mathPow3(int x)
+{
+    int result = x * x * x;
+    return result;
+}
+void printNumbers(int userNumber)
+{
+    int[] kubes = new int[] { };
+    for (int i = 1; i <= userNumber; i++)
+    {
+        Console.Write($"{mathPow3(i)} ");
+
+    }
+}
+
+int userNumber = prompt("таблицу кубов чисел от 1 до ");
+printNumbers(userNumber);
