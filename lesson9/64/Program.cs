50678790ba7612af4b38e66579f6f453a7d7d579
Marek <m.bojarovic@gmail.com>
2023-03-14 11:27:00 +0200

    Задача 66, 68

---
+++ lesson9/64/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int Input(string message)
+{
+    Console.WriteLine(message);
+    int msg = Convert.ToInt32(Console.ReadLine());
+    return msg;
+}
+int b1 = Input("значения N");
+
+string WriteNumber(int number)
+{
+    if (number == 0)
+        return string.Empty;
+        return number + WriteNumber(number - 1);
+   
+}
+ string result = WriteNumber(b1);
+ Console.WriteLine($"N = {b1} {result}");
---
+++ lesson9/66/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int Input(string message)
+{
+    Console.WriteLine(message);
+    int msg = Convert.ToInt32(Console.ReadLine());
+    return msg;
+}
+int a = Input("значения M");
+int b = Input("значения N");
+
+int WriteNumber(int numberM, int numberN)
+{
+   
+    if (numberM == numberN +1) {
+        return 0;
+        } else {
+           return numberM + WriteNumber(numberM +1, numberN); 
+        }
+   
+}
+ int result = WriteNumber(a, b);
+ Console.WriteLine($"N = {b} {result}");
